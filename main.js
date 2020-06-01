37d9e489249c26d335f5d3680b964675c932d5e2
Muhammad Usman <usmanazhoor123@gmail.com>
2020-06-01 09:33:05 +0500

    added some printing lines

--- main.js
+++ main.js
@@ -2,17 +2,23 @@ const RSA = require('./rsa');
 
 const keys = RSA.generateKeys(512);
 
-const message = 'hello world';
+const message = 'hello there i am using RSA Encryption';
+
+console.log(`Original Message: ${message}`);
 
 const encryptedMsg = RSA.encryptMsg(message, {
     e: keys.e,
     n: keys.n
 })
+
+console.log(`Encrypted Messsage: ${encryptedMsg}`);
+
 const decryptedMsg = RSA.decryptMsg(encryptedMsg, {
     d: keys.d,
     n: keys.n,
 })
-console.log(`decryptedMsg: ${decryptedMsg}`);
+
+console.log(`Decrypted Message: ${decryptedMsg}`);
 
 if(message === decryptedMsg) {
     console.log('The Encryption is correct');
